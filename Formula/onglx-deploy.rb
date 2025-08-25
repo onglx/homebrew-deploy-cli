24dbd92188f131edf9550b2b42736f0a2e3e33b5
Njabulo Majozi <services@njabulomajozi.com>
2025-08-25 16:24:09 +0200

    feat: add homebrew formula for OnglX Deploy CLI distribution

---
+++ Formula/onglx-deploy.rb
@@ -0,0 +1,33 @@
+class OnglxDeploy < Formula
+  desc "Deploy AI APIs to AWS in 30 seconds with 85% cost savings"
+  homepage "https://docs.onglx.dev"
+  version "0.1.39"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/onglx/homebrew-deploy-cli/releases/download/v#{version}/onglx-deploy_Darwin_arm64.tar.gz"
+      sha256 "replace_with_darwin_arm64_sha256"
+    else
+      url "https://github.com/onglx/homebrew-deploy-cli/releases/download/v#{version}/onglx-deploy_Darwin_amd64.tar.gz"
+      sha256 "replace_with_darwin_amd64_sha256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/onglx/homebrew-deploy-cli/releases/download/v#{version}/onglx-deploy_Linux_arm64.tar.gz"
+      sha256 "replace_with_linux_arm64_sha256"
+    else
+      url "https://github.com/onglx/homebrew-deploy-cli/releases/download/v#{version}/onglx-deploy_Linux_amd64.tar.gz"
+      sha256 "replace_with_linux_amd64_sha256"
+    end
+  end
+
+  def install
+    bin.install "onglx-deploy"
+  end
+
+  test do
+    system "#{bin}/onglx-deploy", "--version"
+  end
+end
